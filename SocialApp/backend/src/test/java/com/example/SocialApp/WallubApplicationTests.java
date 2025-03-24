55a5c8ff6acf56b5fde049ecbc6aa5ac926f946a
ngnhminh <116098071+ngnhminh@users.noreply.github.com>
2025-03-24 11:17:53 +0700

    Add files via upload

---
+++ SocialApp/backend/src/test/java/com/example/SocialApp/WallubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
