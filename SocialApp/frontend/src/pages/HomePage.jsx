4d02c3c32173f4847f90dda6903e89f53fdab70b
HaiMinh2410 <haiminh24102003@gmail.com>
2025-03-26 17:09:18 +0700

    fe homepage

--- SocialApp/frontend/src/pages/HomePage.jsx
+++ SocialApp/frontend/src/pages/HomePage.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 
 import Sidebar from "../components/Sidebar";
 import Content from "../components/Content";
-import Contact from "../components/Contact";
+import Contacts from "../components/Contacts";
+import { SquarePen } from 'lucide-react';
 
 const HomePage = () => {
 
 
   return (
     <div className="h-screen flex items-center justify-center bg-base-200 ">
-      <div className="rounded-lg shadow-cl w-full h-full flex pt-[56px]">
+      <div className="rounded-lg shadow-cl w-full h-full flex pt-[56px] relative">
         <Sidebar />
         <Content />
-        <Contact />
+        <Contacts />
+        <div className='flex items-center justify-center size-[50px] rounded-full
+          bg-gray-700 hover:bg-gray-600 cursor-pointer absolute bottom-[18px] right-[28px]'>
+          <SquarePen size={20} strokeWidth={2} />
+        </div>
       </div>
     </div>
   )
